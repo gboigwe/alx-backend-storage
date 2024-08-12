0fcb701def5e5917f595a74c430a5b8b396335e4
gboigwe <geakande@gmail.com>
2024-08-12 13:31:16 +0100

    Added country column to table

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Creating another table users if it doesn't exist
+-- Added column country
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
