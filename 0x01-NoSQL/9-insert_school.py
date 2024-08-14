7c52c789bfe7f25fbd5f5815f45bdabbdb922a28
Gbolahan Akande <geakande@gmail.com>
2024-08-14 02:18:04 +0100

    a Python function that insert into documents in a collection

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
-"""Function that inserts a new document in a
-collection based on kwargs
-"""
+""" Function that inserts a new document in a
+collection based on kwargs """
 
 
 def insert_school(mongo_collection, **kwargs):
-    """
-    Function for insertion in a MongoDB collection
-    based on kwargs.
+    """ Function for insertion in a MongoDB collection
+    based on kwargs"""
 
-    Args:
-    mongo_collection: object collection for pymongo
-    **kwargs: Representing keyword arguments.
-    
-    Returns
-    str: new _id of an inserted document
-    """
     return = mongo_collection.insert(kwargs)
