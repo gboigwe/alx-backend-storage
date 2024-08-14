8b206dc56d7cc9b7f6b18a812093c7ab4fd01bf7
Gbolahan Akande <geakande@gmail.com>
2024-08-14 02:11:49 +0100

    a Python function that insert into documents in a collection

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,24 +1,7 @@
 #!/usr/bin/env python3
-"""Function that inserts a new document in a
-collection based on kwargs
-"""
+""" MongoDB Operations with using pymongo """
 
 
 def insert_school(mongo_collection, **kwargs):
-    """
-    Function for insertion in a MongoDB collection
-    based on kwargs.
-
-    Args:
-    mongo_collection: object collection for pymongo
-    **kwargs: Representing keyword arguments.
-    
-    Returns
-    str: new _id of an inserted document
-    """
-    result = mongo_collection.insert_one(kwargs)
-    return str(result.inserted_id)
-
-
-if __name__ == "__main__":
-    pass
+    """ Inserts a new document in a collection based on kwargs """
+    return mongo_collection.insert(kwargs)
---
+++ 0x01-NoSQL/insert_school.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Function that inserts a new document in a
+collection based on kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Function for insertion in a MongoDB collection
+    based on kwargs.
+
+    Args:
+    mongo_collection: object collection for pymongo
+    **kwargs: Representing keyword arguments.
+    
+    Returns
+    str: new _id of an inserted document
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return str(result.inserted_id)
+
+
+if __name__ == "__main__":
+    pass
